548949a5d479579a42cfbc949aaf08c641d6bb3c
Ayush Gaba <ayushgaba71@gmail.com>
2021-12-05 13:15:25 +0530

    changed page names

--- main.js
+++ main.js
@@ -31,18 +31,66 @@ app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
 
-ipc.on('go-terminal', (e) => {
+ipc.on('font-theme', (e) => {
   userchoice = {}
   mainWindow.loadFile('static/defaults.html')
 })
 
-ipc.on('save-terminal', (e, arg) => {
-  userchoice.terminal = arg
-  console.log(userchoice)
+ipc.on('term', (e, arg = null) => {
+  if(!(args === null)){
+    userchoice.font_zshptheme = arg
+    console.log(userchoice)
+  }
   mainWindow.loadFile('static/prompt.html')
 })
 
-ipc.on('save-prompt', (e, arg) => {
-  userchoice.terminal = arg
-  console.log(userchoice)
+ipc.on('zshp', (e, arg = null) => {
+  if(!(args === null)){
+    userchoice.term = arg
+    console.log(userchoice)
+  }
+  
+})
+
+ipc.on('desk_env', (e, arg = null) => {
+  if(!(args === null)){
+    userchoice.zshp = arg
+    console.log(userchoice)
+  }
+  
+})
+
+ipc.on('twm', (e, arg = null) => {
+  if(!(args === null)){
+    userchoice.desk_env = arg
+    console.log(userchoice)
+  }
+})
+
+ipc.on('launcher', (e, arg = null) => {
+  if(!(args === null)){
+    userchoice.twm = arg
+    console.log(userchoice)
+  }
+})
+
+ipc.on('package_install', (e, arg = null) => {
+  if(!(args === null)){
+    userchoice.launcher = arg
+    console.log(userchoice)
+  }
+})
+
+
+ipc.on('dev_tools', (e, arg = null) => {
+  if(!(args === null)){
+    userchoice.package_install = arg
+    console.log(userchoice)
+  }
+})
+
+ipc.on('finish', (e, arg = null) => {
+  if(!(args === null)){
+    userchoice.dev_tools = arg
+  }
 })
