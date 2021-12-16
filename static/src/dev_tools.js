514c2b2558fe7e9256ff503b8b78abe625e09958
Satyam Singh <stymsingh@outlook.com>
2021-12-16 20:40:57 +0530

    Redesign and define developer tools

--- static/src/dev_tools.js
+++ static/src/dev_tools.js
@@ -5,12 +5,22 @@ const prevWindow = document.getElementById('prevbutton')
 
 nextWindow.addEventListener('click', () => {
     data = {};
-    data.vscode = document.getElementById('vscode').checked;
-    data.neovim = document.getElementById('neovim').checked;
+    data.vs_code = document.getElementById('vs-code').checked;
+    data.atom = document.getElementById('atom').checked;
+    data.brackets = document.getElementById('brackets').checked;
     data.geany = document.getElementById('geany').checked;
-    data.sublime = document.getElementById('sublime').checked;
+    data.sublime_text = document.getElementById('sublime-text').checked;
+    data.neovim = document.getElementById('neovim').checked;
+    data.gnu_emacs = document.getElementById('gnu-emacs').checked;
+    data.android_studio = document.getElementById('android-studio').checked;
+    data.clion = document.getElementById('clion').checked;
+    data.intellij_idea_community = document.getElementById('intellij-idea-community').checked;
+    data.intellij_pycharm_community = document.getElementById('intellij-pycharm-community').checked;
+    data.webstorm = document.getElementById('webstorm').checked;
+    data.postman = document.getElementById('postman').checked;
+    data.insomnia = document.getElementById('insomnia').checked;
     ipcRenderer.send('finish', data);
-})  
+})
 
 prevWindow.addEventListener('click', () => {
     ipcRenderer.send('package_install');
