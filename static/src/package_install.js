10048841a1193be119fae54cb9950b93e24e4d9c
Satyam Singh <stymsingh@outlook.com>
2021-12-16 20:40:32 +0530

    Redesign and define package install

--- static/src/package_install.js
+++ static/src/package_install.js
@@ -1,14 +1,23 @@
-var package_install = document.getElementById('package-install')
 var ipcRenderer = require('electron').ipcRenderer
+var package_install = document.getElementById('package-install')
 const nextWindow = document.getElementById('nextbutton')
 const prevWindow = document.getElementById('prevbutton')
 
 nextWindow.addEventListener('click', () => {
     data = {};
-    data.spotify = document.getElementById('spotify').checked;
+    data.chromium = document.getElementById('chromium').checked;
     data.firefox = document.getElementById('firefox').checked;
+    data.spotify = document.getElementById('spotify').checked;
+    data.vlc = document.getElementById('vlc').checked;
+    data.steam = document.getElementById('steam').checked;
     data.discord = document.getElementById('discord').checked;
-    ipcRenderer.send('dev_tools' , data);
+    data.gimp = document.getElementById('gimp').checked;
+    data.obs_studio = document.getElementById('obs-studio').checked;
+    data.zoom = document.getElementById('zoom').checked;
+    data.microsoft_teams = document.getElementById('microsoft-teams').checked;
+    data.telegram = document.getElementById('telegram').checked;
+    data.libreoffice = document.getElementById('libreoffice').checked;
+    ipcRenderer.send('dev_tools', data);
 })
 
 prevWindow.addEventListener('click', () => {
