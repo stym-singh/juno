0227f815f544c7c99027dcb8cde7d14d0b8976dc
Satyam Singh <stymsingh@outlook.com>
2021-12-16 20:39:57 +0530

    Redesign and define program launcher

--- static/src/launcher.js
+++ static/src/launcher.js
@@ -1,12 +1,12 @@
-var program_launcher = document.getElementById('program-launcher')
 var ipcRenderer = require('electron').ipcRenderer
+var program_launcher = document.getElementById('program-launcher')
 const nextWindow = document.getElementById('nextbutton')
 const prevWindow = document.getElementById('prevbutton')
 
 nextWindow.addEventListener('click', () => {
   data = {}
   data.program_launcher = document.getElementById('program-launcher').value
-  ipcRenderer.send('package_install' , data);
+  ipcRenderer.send('package_install', data);
 })
 
 prevWindow.addEventListener('click', () => {
@@ -15,10 +15,13 @@ prevWindow.addEventListener('click', () => {
 
 program_launcher.addEventListener('change', () => {
   if (program_launcher.value == 'rofi') {
-    document.getElementById('ulauncher-info').classList.add('d-none')
-    document.getElementById('rofi-info').classList.remove('d-none')
+    document.getElementById('ulauncher-info').style.display = "none"
+    document.getElementById('rofi-info').style.display = "block"
   } else if (program_launcher.value == 'ulauncher') {
-    document.getElementById('rofi-info').classList.add('d-none')
-    document.getElementById('ulauncher-info').classList.remove('d-none')
+    document.getElementById('rofi-info').style.display = "none"
+    document.getElementById('ulauncher-info').style.display = "block"
+  } else if (program_launcher.value == 'none') {
+    document.getElementById('rofi-info').style.display = "none"
+    document.getElementById('ulauncher-info').style.display = "none"
   }
 })
